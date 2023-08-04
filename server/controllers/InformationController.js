09d4a73545a73af4be2770661346622e02e7c5c4
CristianConcha <ciconcha@miuandes.cl>
2023-08-04 09:54:48 -0400

    feat(Controller): add new Controller and routes

--- server/controllers/InformationController.js
+++ server/controllers/InformationController.js
@@ -4,3 +4,12 @@ const Review = require("../models/Review");
 const Sale = require("../models/Sale");
 
 // Shows authors, number of published books, average score and total sales. This table should have a sort and filter for each column.
+
+// Authors data Table
+// Auhtor name, published books, avg score, total sales
+
+// Top 10 rated Books
+
+// Top 50 selling Books
+
+// Search

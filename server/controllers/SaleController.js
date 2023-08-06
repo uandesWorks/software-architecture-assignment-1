ee58ed60158e0dab23f9efa3cd4470efe26f1975
Milcao <mjanas@miuandes.cl>
2023-08-06 14:49:21 -0400

    feat(Sales): Create and Update auto assign book name

--- server/controllers/SaleController.js
+++ server/controllers/SaleController.js
@@ -5,6 +5,11 @@ const Book = require("../models/Book");
 exports.createSale = async (req, res) => {
   try {
     const sale = new Sale(req.body);
+    const bookName = await Book.findById(sale.book._id, 'name');
+      if (!bookName) {
+        return res.status(404).json({ error: "Book not found" });
+      }
+      sale.book.name = bookName.name;
     await sale.save();
 
     await Book.findByIdAndUpdate(sale.book_id, {
@@ -43,27 +48,32 @@ exports.getSaleById = async (req, res) => {
 // Update a sale by ID
 exports.updateSale = async (req, res) => {
   try {
-    const { book_id, year, sales: newSales } = req.body;
+    const { book, year, sales: newSales } = req.body;
+
+    const updateObj = {book, year, sales: newSales };
 
     const previousSale = await Sale.findById(req.params.id);
     if (!previousSale) {
       return res.status(404).json({ error: "Sale not found" });
     }
 
-    const book = await Book.findById(book_id);
-    if (!book) {
+
+    const newBook = await Book.findById(book._id);
+    if (!newBook) {
       return res.status(404).json({ error: "Book not found" });
     }
+    updateObj.book._id = newBook._id;
+    updateObj.book.name = newBook.name;
 
     const differenceInSales = newSales - previousSale.sales;
 
     const updatedSale = await Sale.findByIdAndUpdate(
       req.params.id,
-      { sales: newSales },
+      updateObj,
       { new: true }
     );
 
-    await Book.findByIdAndUpdate(book_id, {
+    await Book.findByIdAndUpdate(newBook._id, {
       $inc: { sales: differenceInSales },
     });
 
